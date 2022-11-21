8782a232d5cfac2bdf7d80fade46053a01293c31
Ralph Ursprung <ralph.ursprung@gmail.com>
2022-11-21 13:07:50 +0100

    add defmt for loggging over RTT
    use [defmt][] to log over RTT. when launching the application via
    probe-run the logs will be shown directly.
    
    [defmt]: https://defmt.ferrous-systems.com/

--- robotcar/src/main.rs
+++ robotcar/src/main.rs
@@ -3,11 +3,14 @@
 
 use panic_halt as _;
 
+use defmt_rtt as _;
+
 use cortex_m_rt::entry;
 
 use stm32f4xx_hal as _;
 
 #[entry]
 fn main() -> ! {
+    defmt::info!("program started");
     loop {}
 }
