a70706f50ad51f4637740b9de76c7ff5390afc62
Ralph Ursprung <ralph.ursprung@gmail.com>
2022-11-21 13:21:03 +0100

    switch to an RTIC based application
    this makes use of [RTIC][] to add easy task handling for both software
    tasks as well as hardware tasks (interrupts).
    
    [RTIC]: https://rtic.rs/

--- robotcar/src/main.rs
+++ robotcar/src/main.rs
@@ -5,12 +5,27 @@ use panic_probe as _;
 
 use defmt_rtt as _;
 
-use cortex_m_rt::entry;
+#[rtic::app(device = stm32f4xx_hal::pac, dispatchers = [EXTI1])]
+mod app {
+    use stm32f4xx_hal::{pac::TIM5, prelude::*, timer::MonoTimerUs};
 
-use stm32f4xx_hal as _;
+    #[monotonic(binds = TIM5, default = true)]
+    type MicrosecMono = MonoTimerUs<TIM5>;
 
-#[entry]
-fn main() -> ! {
-    defmt::info!("program started");
-    loop {}
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
+        let rcc = ctx.device.RCC.constrain();
+        let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
+        let mono = ctx.device.TIM5.monotonic_us(&clocks);
+
+        defmt::info!("program started");
+
+        (Shared {}, Local {}, init::Monotonics(mono))
+    }
 }
